9ae71b2bc6fd89486bf2ae2b3c364bf4dd9d91da
Jason A. Storey <jason.storey@unrulymedia.com>
2015-03-24 11:34:10 +0000

    Move hypothesis generation into separate module

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-var programSynthesis = {
-    history: {}
-};
+var hypothesisGenerator = require('./lib/hypothesis-generator');
 
-var TOKENS = ['arguments[0]','arguments[1]','arguments[2]','++','--','10','*'];
+var programSynthesis = {};
 
 programSynthesis.generate = function(testData, maxIterations) {
     var MAX_ITERATIONS = maxIterations || 1000000,
         iterationIndex = 0;
 
-    programSynthesis.history = {};
-
-    function generatedFunction(input) { return false; }
+    function generatedFunction(input) {
+        return false;
+    }
 
     while(!checkIfSatisfied(generatedFunction, testData)) {
         if(iterationIndex === MAX_ITERATIONS) {
             throw new Error('Failed to generate a valid hypothesis');
         }
 
-        generatedFunction = generateHypothesis();
+        generatedFunction = hypothesisGenerator.generate();
 
         iterationIndex++;
     }
@@ -30,39 +28,6 @@ function checkIfSatisfied(hypothesis, testData) {
         var args = data.input instanceof Array ? data.input : [data.input];
         return hypothesis.apply(null, args) === data.output;
     });
-}
-
-function generateHypothesis() {
-    try {
-        eval('function hypothesis(input) { return ' + getRandomExpression() + ';}');
-        hypothesis();
-    } catch (e) {
-        return generateHypothesis();
-    }
-
-    return hypothesis;
-}
-
-function getRandomExpression() {
-    var length = Math.ceil(Math.random() * 10),
-        expression = '';
-
-    for(var i = 0; i < length; i++) {
-        expression += getRandomToken();
-        expression += ' ';
-    }
-
-    if(programSynthesis.history[expression] === true) {
-        expression = getRandomExpression();
-    } else {
-        programSynthesis.history[expression] = true;
-    }
-
-    return expression;
-}
-
-function getRandomToken() {
-    return TOKENS[Math.floor(Math.random() * TOKENS.length)];
 }
 
 module.exports = programSynthesis;
