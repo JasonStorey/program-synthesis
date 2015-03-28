0a72d835d58f7d63889ecdbfc9dd466e9b843130
Jason Storey <jason.andrew.storey@gmail.com>
2015-03-28 22:13:25 +0000

    Refactor: move hypothesis testing into the generator

--- index.js
+++ index.js
@@ -3,34 +3,15 @@ var hypothesisGenerator = require('./lib/hypothesis-generator');
 var programSynthesis = {};
 
 programSynthesis.generate = function(testData, maxIterations) {
-    var MAX_ITERATIONS = maxIterations || 1000000,
-        iterationIndex = 0;
-
-    function generatedFunction(input) {
-        return false;
-    }
+    var options = {
+        maxIterations: maxIterations || 1000000,
+        testData: testData
+    };
 
     hypothesisGenerator.reset();
     hypothesisGenerator.analyse(testData);
 
-    while(!checkIfSatisfied(generatedFunction, testData)) {
-        if(iterationIndex === MAX_ITERATIONS) {
-            throw new Error('Failed to generate a valid hypothesis');
-        }
-
-        generatedFunction = hypothesisGenerator.generate();
-
-        iterationIndex++;
-    }
-
-    return generatedFunction;
+    return hypothesisGenerator.generate(options);
 };
-
-function checkIfSatisfied(hypothesis, testData) {
-    return testData.every(function(data) {
-        var args = data.input instanceof Array ? data.input : [data.input];
-        return hypothesis.apply(null, args) === data.output;
-    });
-}
 
 module.exports = programSynthesis;
